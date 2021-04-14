52b31b9607f0cc3215e3dfe8b963f4b454162f68
Andrew Jiang <andrewjiang@MacBook-Pro.lan>
2021-04-14 13:01:52 -0500

    add start script

--- index.js
+++ index.js
@@ -153,6 +153,8 @@ app.delete('/punks/id/:id', async (req, res) =>{
   res.redirect('/punks')
 })
 
-app.listen(3000, () => {
-  console.log("LISTENING ON PORT 3000")
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`LISTENING ON PORT ${port}`)
 })
