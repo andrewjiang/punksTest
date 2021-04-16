792a1308c8b5182c35c57ac03bfc032394e1c348
Andrew Jiang <andrewjiang@MacBook-Pro.lan>
2021-04-16 08:41:49 -0500

    Fix

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 
 const express = require('express')
 const app = express();
@@ -16,18 +18,17 @@ const dbUrl = process.env.DB_URL || "mongodb+srv://our-first-user:1Y2qcVsTdVxvhp
 // mongodb://localhost:27017/punksApp
 
 mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
-.then(() => {
-  console.log("CONNECTION OPEN!! HERE WE ARE!")
-})
-.catch(err => {
-  console.log("ERROR!");
-  console.log(err);
-})
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -170,9 +171,8 @@ app.delete('/punks/id/:id', async (req, res) =>{
   res.redirect('/punks')
 })
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(process.env.PORT)
-  console.log(`LISTENING ON PORT ${port}`)
+    console.log(`Serving on port ${port}`)
 })
